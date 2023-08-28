ca5e4fef0dde1eded758938cc4a3863969098460
nechamafield <nechamafield@gmail.com>
2023-08-28 11:02:54 -0400

    #47 HW17 corrected and moved

---
+++ sprocs/UsersGet.sql
@@ -0,0 +1,27 @@
+--AS When you default a param to blank it shouldn't actually have a blank space rather ie: ''
+--AS Table column params should go before @All
+create or alter procedure dbo.UsersGet (@UsersId int = 0, @Username varchar (30) = '',  @All bit = 0)
+as
+begin
+--AS Add in nullif
+	select @Username = nullif (@UserName, '')
+
+	select u.UsersId, u.UserFirstName, u.UserLastName, u.Username
+	from users u
+	where u.UsersId = @UsersId
+	or @All = 1
+	or u.Username like '%' + @Username+ '%'
+	order by u.UsersId, u.UserFirstName, u.UserLastName, u.Username
+end
+go
+/*
+exec UsersGet
+exec UsersGet  @All = 1
+
+exec UsersGet  @Username = ' ' -- return no results
+exec UsersGet  @Username = 'a'
+
+declare @Id int
+select top 1 @id = u.usersid from users u
+exec UsersGet @Usersid = @Id
+*/
