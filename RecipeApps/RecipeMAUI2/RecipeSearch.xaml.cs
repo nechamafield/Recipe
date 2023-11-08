72461f984c41c4ddbdd323545f93bfa52a410bfd
nechamafield <nechamafield@gmail.com>
2023-11-08 16:08:18 -0500

    recipe search works but list doesnt load

--- RecipeApps/RecipeMAUI2/RecipeSearch.xaml.cs
+++ RecipeApps/RecipeMAUI2/RecipeSearch.xaml.cs
@@ -1,3 +1,6 @@
+using RecipeSystem;
+using System.Data;
+
 namespace RecipeMAUI2;
 
 public partial class RecipeSearch : ContentPage
@@ -6,4 +9,15 @@ public partial class RecipeSearch : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void SearchRecipes()
+    {
+        DataTable dt = Recipe.SearchRecipe(RecipeNameTxt.Text);
+        RecipeLst.ItemsSource = dt.Rows;
+    }
+
+    private void SearchBtn_Clicked(object sender, EventArgs e)
+    {
+        SearchRecipes();
+    }
 }
